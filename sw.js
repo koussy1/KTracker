07a9f2d4a2a2800f36301dbf583fb8e4eadbfc38
koussy1 <koussy@gmail.com>
2026-04-23 23:35:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
 const CACHE_NAME = 'ktracker-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './logo.png'
-];
-
+const ASSETS = ['./', './index.html', './logo.png'];
 self.addEventListener('install', (i) => {
   i.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
-
 self.addEventListener('fetch', (f) => {
   f.respondWith(caches.match(f.request).then((r) => r || fetch(f.request)));
 });
